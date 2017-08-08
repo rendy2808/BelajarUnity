9889f803be36e0ce728954e71807b769575db66b
garudamaya <group.garudamaya@gmail.com>
2017-08-08 14:46:00 +0700

    test

---
+++ BananaTouch/Assets/BananaHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BananaHandler : MonoBehaviour {
+
+	public float totalBan;
+	public float bps;
+	public Text totalBanTxt;
+	public Text bpsTxt;
+
+	public float updateTime;
+	float counter;
+
+	public bool minimumUpdateTime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (minimumUpdateTime)
+			updateTime = Time.deltaTime;
+
+		bpsTxt.text = bps + " Bananas/Second";
+		totalBanTxt.text = totalBan.ToString ("F1") + " Bananas";
+
+		if (counter < updateTime)
+			counter += Time.deltaTime;
+		else {
+			totalBan += bps * updateTime;
+			counter = 0;
+		}
+		
+	}
+}
---
+++ BananaTouch/Assets/TouchStuff.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchStuff : MonoBehaviour {
+
+	int i;
+	public int maxFinger;
+	int fingerCount;
+	public Text score; 
+
+	public GameObject cut;
+
+
+
+	// Use this for initialization
+	Vector3 worldPos;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began && fingerCount < maxFinger) {
+				i++;
+				this.GetComponent<Animator> ().SetBool ("Tapped", true);
+				worldPos = Camera.main.ScreenToWorldPoint (touch.position);
+				worldPos.z = 0;
+
+				Instantiate (cut,worldPos,Quaternion.Euler(0,0,Random.Range(80,100)));
+			}
+			fingerCount++;
+		}
+		score.text = i.ToString ();
+		
+	}
+	public void TappedFalse(){
+		this.GetComponent<Animator> ().SetBool ("Tapped", false);
+	
+	}
+}
